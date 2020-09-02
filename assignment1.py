fe5086c7e23a624191cb045e9dca60c4854b36a8
Chailex <32385484+Chailex@users.noreply.github.com>
2020-09-02 16:00:43 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  2 12:51:33 2020
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
-@author: user
-"""
+#Loading the dataset
+pcaData = pd.read_csv("PCA_practice_dataset.csv")
+X=pcaData.to_numpy()
+print(X.shape)
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+#PCA
+pca = PCA()
+X = pca.fit_transform(X)
+
+cumalitiveVarience = np.cumsum(pca.explained_variance_ratio_)*100
+thresholds = [i for i in range(90,98)]
+
+components = [np.argmax(cumalitiveVarience>threshold) for threshold in thresholds]
+
+for component, threshold in zip(components, thresholds):
+    print("Component for threshold"+str(threshold)+"% is :",component)
+      
+
+#dimensionality reduction
+A=X
+for component, threshold in zip(components, thresholds):
+    pca=PCA(n_components=component)
+    newX=pca.fit_transform(A)
+    print("\nThreshold: ", threshold)
+    print("Shape after dimensionalty reduction: ", newX.shape)
+    
+#Plotting the data on scree plot
+plt.ylabel("Threshold")
+plt.xlabel("Principle component")
+plt.plot(components,range(90,98),'o-') 
 
